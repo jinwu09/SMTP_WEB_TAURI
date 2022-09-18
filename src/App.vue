124d90e794e1fa0e54a548ed486350ef95bc73ec
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-09-18 15:47:29 +0800

    update ui

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
 
   <main>
     <div class="">
-      <div class="mx-auto my-20 bg-gray-400 w-2/3 p-4">
+      <div class="mx-auto m bg-gray-400 w-2/3 p-4">
         <form action="">
           <div class="grid grid-cols-[80px_auto_80px_auto] gap-y-4 gap-1">
             <p>Server:</p>
@@ -34,13 +34,18 @@ export default {
               type="text"
               placeholder="ex smtp.gmail.com"
               v-model="SenderInfo.server"
+              required
             />
             <p>Port</p>
-            <input type="text" v-model="SenderInfo.port" />
+            <input type="text" v-model="SenderInfo.port" required />
             <p>Username:</p>
-            <input type="email" v-model="SenderInfo.auth.email" />
+            <input type="email" v-model="SenderInfo.auth.email" required />
             <p>Password:</p>
-            <input type="password" v-model="SenderInfo.auth.password" />
+            <input
+              type="password"
+              v-model="SenderInfo.auth.password"
+              required
+            />
           </div>
           <div>
             <p>Message:</p>
@@ -49,10 +54,16 @@ export default {
               <input type="text" v-model="Message.subject" />
               <p></p>
               <p>Message:</p>
-              <input type="text" v-model="Message.text" />
+              <input type="text" v-model="Message.text" class="h-6" />
               <p></p>
               <p>HTML</p>
-              <input type="text" v-model="Message.html" />
+              <textarea
+                name=""
+                cols="50"
+                rows="10"
+                v-model="Message.html"
+                class="resize-none"
+              ></textarea>
             </div>
           </div>
         </form>
