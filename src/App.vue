a680833917e536f325683bca699f782b8bf3e0e6
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-09-18 15:21:03 +0800

    ui good next is using nodemailer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      SenderInfo: {
+        server: "",
+        port: "",
+        auth: {
+          email: "",
+          password: "",
+        },
+      },
+      Message: {
+        subject: "",
+        text: "",
+        html: "",
+      },
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <header></header>
 
   <main>
-    <TheWelcome />
+    <div class="">
+      <div class="mx-auto my-20 bg-gray-400 w-2/3 p-4">
+        <form action="">
+          <div class="grid grid-cols-[80px_auto_80px_auto] gap-y-4 gap-1">
+            <p>Server:</p>
+            <input
+              type="text"
+              placeholder="ex smtp.gmail.com"
+              v-model="SenderInfo.server"
+            />
+            <p>Port</p>
+            <input type="text" v-model="SenderInfo.port" />
+            <p>Username:</p>
+            <input type="email" v-model="SenderInfo.auth.email" />
+            <p>Password:</p>
+            <input type="password" v-model="SenderInfo.auth.password" />
+          </div>
+          <div>
+            <p>Message:</p>
+            <div class="grid grid-cols-[80px_auto_50px] gap-y-4">
+              <p>Subject:</p>
+              <input type="text" v-model="Message.subject" />
+              <p></p>
+              <p>Message:</p>
+              <input type="text" v-model="Message.text" />
+              <p></p>
+              <p>HTML</p>
+              <input type="text" v-model="Message.html" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
