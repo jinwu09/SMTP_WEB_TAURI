81d4f12d08100e65ee79e6e2397bd650e707a97c
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-09-19 23:44:12 +0800

    Email UI added, nodemailer done
    front end needs more fix to have good UX, backend is good except for database

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,9 +1,49 @@
+const { json } = require("express");
 var express = require("express");
+const nodemailer = require("nodemailer");
 var router = express.Router();
+
+var hello = {
+  h1: "hi there",
+  h2: {
+    hi: "hi",
+  },
+};
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "hello" });
+  res.send(JSON.stringify(hello.h1 + hello.h2.hi));
+  // res.send(JSON.stringify(hello));
+  // res.render("index", { title: "hello" });
 });
+router.get("/hello", function (req, res, next) {
+  res.send(JSON.stringify(hello));
+});
+router.post("/sendmail", function (req, res) {
+  let content = req.body;
 
+  let transporter = nodemailer.createTransport({
+    host: content.account.host,
+    port: content.account.port,
+    secure: content.account.secure,
+    auth: {
+      user: content.account.auth.user,
+      pass: content.account.auth.pass,
+    },
+  });
+  let info = transporter.sendMail({
+    from: content.send.from,
+    to: content.send.to,
+    subject: content.send.subject,
+    text: content.send.text,
+    html: content.send.html,
+  });
+  let status = {
+    code: 200,
+    messageid: info.messageid,
+    content: content,
+  };
+  res.status(status.code).json({ status });
+  // res.sendStatus(200);
+});
 module.exports = router;
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ export default {
   data() {
     return {
       SenderInfo: {
-        server: "",
+        host: "",
         port: "",
         auth: {
           email: "",
@@ -16,9 +16,18 @@ export default {
         text: "",
         html: "",
       },
+      AddingEmail: "",
+      Emails: [],
     };
   },
-  methods: {},
+  methods: {
+    AddedEmail() {
+      this.Emails.push(this.AddingEmail);
+    },
+    DeleteEmail(index) {
+      this.Emails.splice(index, 1);
+    },
+  },
 };
 </script>
 
@@ -27,44 +36,63 @@ export default {
 
   <main>
     <div class="">
-      <div class="mx-auto mt-2 bg-gray-400 w-2/3 p-4">
-        <form action="">
-          <div class="grid grid-cols-[80px_auto_80px_auto] gap-y-4 gap-1">
-            <p>Server:</p>
+      <div class="mx-auto gap-3 mt-2 bg-gray-400 p-4 grid grid-cols-[30%_auto]">
+        <div>
+          <div class="grid grid-cols-[auto_50px]">
             <input
-              type="text"
-              placeholder="ex smtp.gmail.com"
-              v-model="SenderInfo.server"
+              type="email"
+              placeholder="sample@email.com"
+              v-model="AddingEmail"
               required
             />
-            <p>Port</p>
-            <input type="text" v-model="SenderInfo.port" required />
-            <p>Username:</p>
-            <input type="email" v-model="SenderInfo.auth.email" required />
-            <p>Password:</p>
-            <input
-              type="password"
-              v-model="SenderInfo.auth.password"
-              required
-            />
+            <button @click="AddedEmail()" class="bg-green-400">add</button>
           </div>
+          <div v-for="(Email, index) in Emails" v-bind:key="Email">
+            <p>{{ Email }} {{ index }}</p>
+            <button class="bg-red-400" @click="DeleteEmail(index)">
+              Delete
+            </button>
+          </div>
+        </div>
+        <form action=" " class="">
           <div>
-            <p>Message:</p>
-            <div class="grid grid-cols-[80px_auto_50px] gap-y-4">
-              <p>Subject:</p>
-              <input type="text" v-model="Message.subject" />
-              <p></p>
+            <div class="grid grid-cols-[80px_auto_80px_auto] gap-y-4 gap-1">
+              <p>host:</p>
+              <input
+                type="text"
+                placeholder="ex smtp.gmail.com"
+                v-model="SenderInfo.host"
+                required
+              />
+              <p>Port</p>
+              <input type="text" v-model="SenderInfo.port" required />
+              <p>Username:</p>
+              <input type="email" v-model="SenderInfo.auth.email" required />
+              <p>Password:</p>
+              <input
+                type="password"
+                v-model="SenderInfo.auth.password"
+                required
+              />
+            </div>
+            <div>
               <p>Message:</p>
-              <input type="text" v-model="Message.text" class="h-6" />
-              <p></p>
-              <p>HTML</p>
-              <textarea
-                name=""
-                cols="50"
-                rows="10"
-                v-model="Message.html"
-                class="resize-none"
-              ></textarea>
+              <div class="grid grid-cols-[80px_auto_50px] gap-y-4">
+                <p>Subject:</p>
+                <input type="text" v-model="Message.subject" />
+                <p></p>
+                <p>Message:</p>
+                <input type="text" v-model="Message.text" class="h-6" />
+                <p></p>
+                <p>HTML</p>
+                <textarea
+                  name=""
+                  cols="50"
+                  rows="10"
+                  v-model="Message.html"
+                  class="resize-none"
+                ></textarea>
+              </div>
             </div>
           </div>
         </form>
